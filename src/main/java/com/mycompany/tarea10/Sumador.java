d45dc90ce88b4a8c8a0d83182a872826c2c5ed14
Guille <Guille@gmail.com>
2022-03-22 11:40:40 +0100

    Primera version

---
+++ src/main/java/com/mycompany/tarea10/Sumador.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea10;
+
+/**
+ *
+ * @author ESDPC
+ */
+public class Sumador {
+public static float sumar (float a, float b ) {
+return a + b;
+}}
